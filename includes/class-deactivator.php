32872b1aa5168cd1ddf05b003e7e5de5cd5b502d
nuzwa269 <blsoha424@gmail.com>
2026-03-16 10:35:39 +0500

    Create APFA_Deactivator class for plugin deactivation
    Added a class for handling plugin deactivation and cleanup tasks.

---
+++ includes/class-deactivator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Deactivator
+ * Handles cleanup on plugin deactivation
+ */
+
+class APFA_Deactivator {
+
+    public static function deactivate() {
+        // Optional: Add cleanup tasks here
+        // Note: Do NOT delete tables on deactivation
+        // Users' data should be preserved
+    }
+}
